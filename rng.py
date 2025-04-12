23755ef8c44acc830f12d57d928ddccd4733ba8c
stinkylynx1 <70523499+stinky-lynx1@users.noreply.github.com>
2025-04-13 00:04:47 +0300

    Update rng.py

--- rng.py
+++ rng.py
@@ -1,14 +1,14 @@
-import random # imports the random command
+import random
 
-gend = input("Ahoi! You a boy, girl or void? ").lower() # checks ur GENDAH
+gend = input("Ahoi! You a boy, girl or void? ").lower()
 
-print(f"Good {gend}! Now input 2 numbers. :3") # heheehe
+print(f"Good {gend}! Now input 2 numbers. :3")
 
-a = int(input("Your first number: ")) # takes input for the first number
+a = int(input("Your first number: "))
 
-b = int(input("Your second number: ")) # takes input for the second number
+b = int(input("Your second number: "))
 
-if a > b:   # compares the numbers to see if a is bigger than b, in that case it auto shuts down
-    print(f"Bad {gend}! 3:") 
-else:   # if the numbers are correct ur getting an output!
-    print("Your silly number is: ", random.randint(a, b)) # prints the random number
+if a > b:
+    print(f"Bad {gend}! 3:")
+else:
+    print("Your silly number is: ", random.randint(a, b))
