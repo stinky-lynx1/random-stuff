50771caf31b054aa6ea461159dab760545abc27c
stinkylynx1 <70523499+stinky-lynx1@users.noreply.github.com>
2025-06-24 13:08:56 +0300

    Update intecalc.py

--- intecalc.py
+++ intecalc.py
@@ -2,12 +2,16 @@ import math # just in case
 
 a, b, dx, i, r, func = float(input("Upper bound a=")), float(input("Lower bound b=")), float(input("Variable dx=")), 0, int(input("Digits after decimal point n=")), input("f(x)=")
 
-x1 = b
-while x1 <= a:
-    c = lambda x : eval(func)
-    y = c(x1)
-    print("y=" + str(y), "x=" + str(x1))
-    i += y * dx
-    x1 += dx
-
-print("The integral for your function and parameters is f(x)=" + str(round(i,r)))
+if a < b:
+    print("Integral uncalculable! Upper bound must be bigger than lower bound!")
+elif a == b:
+    print("Integral uncalculable! Upper and lower bounds must have a different value!")
+else:
+    x1 = b
+    while x1 <= a:
+        c = lambda x : eval(func)
+        y = c(x1)
+        i += y * dx
+        x1 += dx
+        print("y=" + str(y), "x=" + str(x1))
+    print("The integral for your function and parameters is f(x)=" + str(round(i,r)))
