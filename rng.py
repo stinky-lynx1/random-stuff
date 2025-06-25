f56594b3874bc1a2acb04110576ccb8372daef77
stinkylynx1 <70523499+stinky-lynx1@users.noreply.github.com>
2025-06-25 03:49:05 +0300

    Update rng.py

--- rng.py
+++ rng.py
@@ -1,30 +1,33 @@
-import random
+import random # imports the random command
 
-amber, ruby, esther = 0, 0, 0
+amber, ruby, esther, terra = 0, 0, 0, 0
 
-gend = input("Ahoi! You a boy, girl or void? ").lower()
+gend = input("Ahoi! You a boy, girl or void?\n").lower() 
 if gend == "":
     gend = "agender"
 elif gend == "girl":
     print("Amber approves!")
     amber = 1
-    
-print(f"Good {gend}! Now input 2 numbers. :3")
 
-a = int(input("Your first number: "))
+print(f"Good {gend}! Now input 2 numbers. :3") # heheehe
+
+a = int(input("Your first number:\n")) # takes input for the first number
 if a == 42:
     print("Esther?!")
     esther = 1
 
-b = int(input("Your second number: "))
+b = int(input("Your second number:\n")) # takes input for the second number
 
-if a > b:
-    print(f"Bad {gend}! 3:")
-elif a < b:
-    print("Your silly number is: ", random.randint(a, b))
+if a > b:   # compares the numbers to see if a is bigger than b
+    print(f"Bad {gend}! 3:") 
+elif a < b:   # if the numbers are correct ur getting an output!
+    print("Your silly number is:\n", random.randint(a, b)) # prints the random number
 else:
     print("RUBYYYYYYYYYYYYYY!!!!")
     ruby = 1
+    i = input()
+    if i == "poof":
+        terra = 1
 
-if (ruby == 1 & esther == 1 & amber == 1):
+if (ruby == 1 & esther == 1 & amber == 1 & terra == 1):
     print("OMG OMG YOU COLLECTED ALL THE FAMILY! JADE WILL BE SO PROUD ///^///w///^///")
