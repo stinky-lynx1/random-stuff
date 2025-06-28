0aec1308de0a13d0e6d090bdc5636d1a38127a04
stinkylynx1 <70523499+stinky-lynx1@users.noreply.github.com>
2025-06-28 09:02:56 +0300

    qol and support for threading in the future

--- intecalc.py
+++ intecalc.py
@@ -1,17 +1,38 @@
-import math # just in case 
+from math import *  # for user-implemented functions
+from threading import *  # threading gon kill me istg
+from time import *  # for timer
 
-a, b, dx, i, r, func = float(input("Upper bound a=")), float(input("Lower bound b=")), float(input("Variable dx=")), 0, int(input("Digits after decimal point n=")), input("f(x)=")
+a, x1, dx, r, func = (
+    float(input("Upper bound a=")),
+    float(input("Lower bound b=")),
+    float(input("Variable dx=")),
+    int(input("Digits after decimal point n=")),
+    input("f(x)="),
+)
 
-if a < b:
+if a < x1:
     print("Integral uncalculable! Upper bound must be bigger than lower bound!")
-elif a == b:
+elif a == x1:
     print("Integral uncalculable! Upper and lower bounds must have a different value!")
 else:
-    x1 = b
-    while x1 <= a:
-        c = lambda x : eval(func)
-        y = c(x1)
-        i += y * dx
-        x1 += dx
-        print("y=" + str(y), "x=" + str(x1))
-    print("The integral for your function and parameters is f(x)=" + str(round(i,r)))
+    def integrate(a, x1, dx, r, func):
+        i, cx = 0, 0
+        tstart = time()
+        while x1 <= a:
+            c = lambda x: eval(func)  # necessary evil
+            y = c(x1)
+            i += y * dx
+            print("y=" + str(y), "x=" + str(x1))
+            cx += 1
+            x1 += dx
+        tend = time()
+        tdelta = tend - tstart
+        print(
+            "The integral for your function and parameters is f(x)="
+            + str(round(i, r))
+            + ", calculated in "
+            + str(tdelta)
+            + " seconds and "
+            + str(cx)
+            + " cycles."
+        )
